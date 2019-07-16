8952596a64fef5e05d34416b7e3510494cfae5bc
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-16 10:02:12 -0400

    72 tests passed at 10:02

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -5,31 +5,40 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value*value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double [] newarr=new Double[value.length];
+        for(int x=0;x<newarr.length;x++) {
+            newarr[x] = Math.sqrt(value[x]);
+        }
+        return newarr;
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        Double [] newarr=new Double[values.length];
+        for(int x=0;x<newarr.length;x++){
+            newarr[x]=values[x]*values[x];
+        }
+        return newarr;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1+value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1-value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,104 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+   int position=0;
+   if(string.length()%2==0)
+       position=string.length()/2-1;
+        else
+       position=string.length()/2;
+        char x=(string.substring(position,position+1)).charAt(0);
+        return x;
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+ if(str.length()==1)
+     return str.toUpperCase();
+ else{
+
+        int l=0;
+        int position=0;
+        if(str.length()%2==0){
+            position=str.length()/2-1;
+            l=2;}
+        else {
+            position = str.length() / 2;
+            l=1;
+        }
+        String x=str.substring(0,position)+(str.substring(position,position+l)).toUpperCase()+str.substring(2, str.length());
+
+        return   x;}
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        if(str.length()==1)
+            return str.toLowerCase();
+        else{
+
+            int l=0;
+            int position=0;
+            if(str.length()%2==0){
+                position=str.length()/2-1;
+                l=2;}
+            else {
+                position = str.length() / 2;
+                l=1;
+            }
+            String x=str.substring(0,position)+(str.substring(position,position+l)).toLowerCase()+str.substring(position+1, str.length());
+
+            return   x;}
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        String string=str.toUpperCase();
+        char [] c=str.toCharArray();
+
+         Arrays.sort(c);
+        for(int t=0;t<c.length-1;t++){
+            if(c[t]==c[t+1])
+                return false;
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char [] c=str.toCharArray();
+        for(int t=0;t<c.length-1;t++){
+            if(c[t]==c[t+1])
+                return true;
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        char [] c=str.toCharArray();
+        String string="";
+        String y="";
+        for(int t=0;t<c.length-1;t++){
+            if(c[t]==c[t+1]){
+                y+=c[t]+c[t+1];
+            t++;}
+            else
+                string+=c[t];
+        }
+        String res=string+str.substring(str.length()-1);
+        return res;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        char [] c=str.toCharArray();
+        for(int i=0;i<c.length;i++){
+            char x=c[i];
+            if(Character.isUpperCase(x))
+                c[i]=Character.toLowerCase(x);
+            else if(Character.isLowerCase(x))
+                c[i]=Character.toUpperCase(x);
+        }
+        return new String(c);
     }
 }
