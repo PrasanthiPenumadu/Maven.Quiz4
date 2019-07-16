ccd915a7991a37c63077bb1424438817743137e1
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-16 10:12:53 -0400

    88 tests passed at 10:12

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,40 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        int x=values.length/2;
+        if(values.length%2==0)
+            return values[x-1];
+        else
+        return values[x];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        int x=values.length/2;
+        int j=0;
+        if(values.length%2==0)
+        j=x-1;
+        else
+            j=x;
+        String [] arr=new String[values.length-1];
+        for(int i=0;i<x;i++){
+            arr[i]=values[i];
+        }
+        for(int i=x+1;i<values.length;i++)
+        {
+            arr[i-1]=values[i];
+        }
+        return arr;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String [] arr=new String[values.length-1];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=values[i];
+        }
+        return arr;
     }
 }
